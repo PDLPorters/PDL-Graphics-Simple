0d4435732b931f9bfd737e2b49c60bdebed84ef5
Craig DeForest <zowie@Clio-4.local>
2013-03-16 01:17:31 -0600

    colors and plot holding work now.  (Line styles, not so much yet)

--- lib/PDL/Graphics/Simple/Prima.pm
+++ lib/PDL/Graphics/Simple/Prima.pm
@@ -15,7 +15,6 @@ package PDL::Graphics::Simple::Prima;
 
 use PDL;
 use PDL::Options q/iparse/;
-
 our $mod = {
     shortname => 'prima',
     module => 'PDL::Graphics::Simple::Prima',
@@ -138,8 +137,8 @@ sub DESTROY {
 }
 
 
-# [Need to implement colors too.]
-our @colors = ();   # What goes in here?  I don't know yet.
+our @colors =qw/
+    cl::Black cl::Red cl::Green cl::Blue cl::Cyan cl::Magenta cl::Yellow cl::Brown cl::LighttRed cl::LightGreen cl::LightBlue cl::Gray/;
  
 ##############################
 # Plot types
@@ -147,14 +146,41 @@ our @colors = ();   # What goes in here?  I don't know yet.
 # This probably needs a little more smarts.  
 # Currently each entry is either a ppair::<foo> return or a sub that implements
 # the plot type in terms of others. 
+
 our $types = {
     lines => ppair::Lines,
     points => [ map { 'ppair::'.$_ } qw/Blobs Triangles Squares Crosses Xs Asterisks/ ],
-    bins => undef,
-    errorbars => undef,
+    bins => sub {
+	my ($me, $plot, $block) = @_;
+	my $x = $block->[0];
+	my $x1 = $x->range( [[0],[-1]], [$x->dim(0)], 'e' )->average;
+	my $x2 = $x->range( [[1],[0]],  [$x->dim(0)], 'e' )->average;
+	my $newx = pdl($x1, $x2)->mv(-1,0)->clump(2)->sever;
+
+	my $y = $block->[1];
+	my $newy = $y->dummy(0,2)->clump(2)->sever;
+	
+	$plot->dataSets()->{ 1+keys(%{$plot->dataSets()}) } = 
+	    ds::Pair($newx,$newy,plotType=>eval q{ppair::Lines}, color=> eval $colors[$me->{curvestyle} % @colors]);
+    },
+    errorbars=> undef,
     limitbars => undef,
     image => undef,
-    circles => undef,
+    circles => sub {
+	my($me, $plot, $data) = @_;
+	our $cstash;
+	unless(defined($cstash)) {
+	    my $ang = PDL->xvals(362)*3.14159/180;
+	    $cstash = {};
+	    $cstash->{c}   = $ang->cos;
+	    $cstash->{s}   = $ang->sin;
+	    $cstash->{s}->slice("361") .= $cstash->{c}->slice("361") .= PDL->pdl(1.1)->acos; # NaN
+	}
+	my $dr = $data->[2]->flat;
+	my $dx = ($data->[0]->flat->dummy(0,1) + $dr->dummy(0,1)*$cstash->{c})->flat;
+	my $dy = ($data->[1]->flat->dummy(0,1) + $dr->dummy(0,1)*$cstash->{s})->flat;
+	$plot->dataSets()->{ 1+keys(%{$plot->dataSets()}) } = ds::Pair($dx, $dy, plotType=>eval q{ppair::Lines}, color=> eval $colors[$me->{curvestyle}%@colors]);
+    },
     labels => undef
 };
 
@@ -182,20 +208,21 @@ sub plot {
     
     if(!defined($ipo->{multi})) {
 
-	my @plot_args = ();
 
-	my $plot = $me->{obj}->insert('Plot',
+	my $plot;
+
+	if($ipo->{oplot} and defined($me->{last_plot})) {
+	    $plot = $me->{last_plot};
+	} else {
+	    $plot = $me->{obj}->insert('Plot',
 				      pack=>{fill=>'both',expand=>1}
 	    );
+	}
 	push(@{$me->{widgets}}, $plot);
+	$me->{last_plot} = $plot;
 
 	for my $block(@_) {
 	    my $co = shift @$block;
-
-	    # Get data and generate sequence if necessary
-	    if(@$block < 2) {
-		unshift(@$block, sequence($block->[0]));
-	    }
 
 	    # Parse out curve style (for points type selection)
 	    if(defined($co->{style}) and $co->{style}) {
@@ -204,11 +231,9 @@ sub plot {
 		$me->{curvestyle}++;
 	    }
 	    
-	    my %plot_args;
-
 	    my $type = $types->{$co->{with}};
 	    if( ref($type) eq 'CODE' ) {
-		&{$type}($me, $block);
+		&{$type}($me, $plot, $block);
 	    } else {
 		my $pt;
 		if(ref($type) eq 'ARRAY') {
@@ -218,23 +243,21 @@ sub plot {
 		    $pt = eval qq{$type};
 		}
 
-		$plot->dataSets()->{ 1+keys(%{$plot->dataSets()}) } = ds::Pair(@$block, plotType => $pt);
+		$plot->dataSets()->{ 1+keys(%{$plot->dataSets()}) } = ds::Pair(@$block, plotType => $pt, color=>eval $colors[$me->{curvestyle}%@colors]);
 	    }
 	}
 
 
-	## Set global plot options like title
-	$plot->title($ipo->{title})     if(defined($ipo->{title}));
-	$plot->x->label($ipo->{xlabel}) if(defined($ipo->{xlabel}));
-	$plot->y->label($ipo->{ylabel}) if(defined($ipo->{ylabel}));
+	## Set global plot options: titles, axis labels, and ranges.
+	$plot->title(     $ipo->{title}   )  if(defined($ipo->{title}));
+	$plot->x->label(  $ipo->{xlabel}  )  if(defined($ipo->{xlabel}));
+	$plot->y->label(  $ipo->{ylabel}  )  if(defined($ipo->{ylabel}));
 
 	$plot->x->min($ipo->{xrange}->[0]) if(defined($ipo->{xrange}) and defined($ipo->{xrange}->[0]));
 	$plot->x->max($ipo->{xrange}->[1]) if(defined($ipo->{xrange}) and defined($ipo->{xrange}->[1]));
 
 	$plot->y->min($ipo->{yrange}->[0]) if(defined($ipo->{yrange}) and defined($ipo->{yrange}->[0]));
 	$plot->y->max($ipo->{yrange}->[1]) if(defined($ipo->{yrange}) and defined($ipo->{yrange}->[1]));
-
-
 	
 	Prima::Timer->create(
 	    onTick=>sub{$_[0]->stop; die "done with event loop\n"},

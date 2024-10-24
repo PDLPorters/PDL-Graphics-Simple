409912bada6c65049ad7158fef5762fa99e4bed4
Ed J <mohawk2@users.noreply.github.com>
2024-10-24 23:34:45 +0000

    1.014

--- lib/PDL/Graphics/Simple.pm
+++ lib/PDL/Graphics/Simple.pm
@@ -271,7 +271,7 @@ use PDL::Options q/iparse/;
 use File::Temp qw/tempfile tempdir/;
 use Scalar::Util q/looks_like_number/;
 
-our $VERSION = '1.013';
+our $VERSION = '1.014';
 $VERSION =~ s/_//g;
 
 ##############################
